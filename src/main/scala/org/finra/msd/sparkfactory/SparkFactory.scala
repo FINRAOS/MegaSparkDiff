24f5414a9d386248931228c417e047bef28d1fa9
Sy, Andrew <Andrew.Sy@finra.org>
2019-10-18 15:34:52 -0400

    parallelizeDelimitedFileWithDdl

--- src/main/scala/org/finra/msd/sparkfactory/SparkFactory.scala
+++ src/main/scala/org/finra/msd/sparkfactory/SparkFactory.scala
@@ -21,6 +21,7 @@ import org.apache.hadoop.dynamodb.DynamoDBItemWritable
 import org.apache.hadoop.dynamodb.read.DynamoDBInputFormat
 import org.apache.hadoop.io.Text
 import org.apache.hadoop.mapred.JobConf
+import org.apache.spark
 import org.apache.spark.SparkConf
 import org.apache.spark.rdd.RDD
 import org.apache.spark.sql._
@@ -99,6 +100,41 @@ object SparkFactory {
     val df: DataFrame = sparkSession.sqlContext.createDataFrame(rowRDD, schema).toDF
     return df
   }
+
+  /**
+    * Create DataFrame from a delimited text file.
+    * The file can be on local machine or HDFS or other file system supported by the Spark implementation.
+    * "hdfs://nn1home:8020/input/war-and-peace.txt" for S3 the url like so
+    * "s3n://myBucket/myFile1.log"
+    *
+    * @param fileLocation path of file
+    * @param delimiter delimiter used in the delimited file
+    * @return DataFrame created from the file
+    */
+  def parallelizeDelimitedFile(fileLocation: String, delimiter: String = ","): DataFrame = {
+    sparkSession.read
+      .option("delimiter", delimiter)
+      .csv(fileLocation)
+  }
+
+  /**
+    * Create DataFrame from a delimited text file, applying the specified schema.
+    * The file can be on local machine or HDFS or other file system supported by the Spark implementation.
+    * "hdfs://nn1home:8020/input/war-and-peace.txt" for S3 the url like so
+    * "s3n://myBucket/myFile1.log"
+    *
+    * @param fileLocation path of file
+    * @param delimiter delimiter used in the delimited file
+    * @param ddl schema specified in DDL style, e.g. "name VARCHAR(50), age INT"
+    * @return DataFrame created from the file
+    */
+  def parallelizeDelimitedFileWithDdl(fileLocation: String, ddl: String, delimiter: String = ","): DataFrame = {
+    sparkSession.read
+      .option("delimiter", delimiter)
+      .schema(ddl)
+      .csv(fileLocation)
+  }
+
 
   /**
     * This method will create DataFrame with a single column called "field1" from a text file, the file can be on local
